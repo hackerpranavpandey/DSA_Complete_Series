646afaca38a83ee22104fa11eb8d263383bc902e
Pranav Kumar Pandey <220010043@iitdh.ac.in>
2024-08-04 12:43:47 +0530

    some more files or code in recursion

--- 10_Recursion/11_palindrome.cpp
+++ 10_Recursion/11_palindrome.cpp
@@ -1,19 +1,19 @@
 //checking if a string is palindrome
 #include<bits/stdc++.h>
 using namespace std;
-bool check_palindrome(string& t,int start,int end){
+bool check(string& t,int start,int end){
     // base condition
     if(start>=end)
         return true;
     if(t[start]!=t[end])
         return false;
     else 
-         return check_palindrome(t,start+1,end-1);
+        return check(t,start+1,end-1);
 }
 int main(){
     string s;
     cin>>s;
-    bool ans=check_palindrome(s,0,s.size()-1);
+    bool ans=check(s,0,s.size()-1);
     cout<<ans<<endl;
     return 0;
 } 
